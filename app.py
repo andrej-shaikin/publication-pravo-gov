0c43f79eb9278d66442637f9b30a14200f6981a3
Andrey Shaikin <kiwibon@yandex.ru>
2022-09-12 20:05:32 +0300

    sqlalchemy => ormar

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
-from fastapi_sqlalchemy import DBSessionMiddleware
 
 from apps.router import router
 from conf import settings
 
 app = FastAPI(debug=settings.DEBUG)
-
-app.add_middleware(DBSessionMiddleware, db_url=settings.DATABASE_DSN)
 app.include_router(router=router)
+app.state.database = settings.db
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    database_ = app.state.database
+    if not database_.is_connected:
+        await database_.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    database_ = app.state.database
+    if database_.is_connected:
+        await database_.disconnect()
--- apps/document_types/logic/actions.py
+++ apps/document_types/logic/actions.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter
-from fastapi_sqlalchemy import db
-from sqlalchemy_get_or_create import update_or_create
 
 from apps.document_types.logic.getters import get_document_types_from_source
 from apps.document_types.models import DocumentType
@@ -10,10 +8,4 @@ router = APIRouter()
 
 async def refresh_document_types_from_source() -> None:
     for document_type in await get_document_types_from_source():
-        update_or_create(
-            session=db.session,
-            model=DocumentType,
-            uuid=document_type.uuid,
-            defaults={"name": document_type.name},
-        )
-        db.session.commit()
+        await DocumentType.objects.update_or_create(uuid=document_type.uuid, defaults={"name": document_type.name})
